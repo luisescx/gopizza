448ab6e0bbf5489e2e8f9bdf0c0e0d5b48720cb7
luisescx <luis.esculapio@Live.com>
2022-01-16 21:40:17 -0300

    Feat: added forwardRef on input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { TextInputProps } from "react-native";
+import React, { forwardRef } from "react";
+import { TextInput, TextInputProps } from "react-native";
 
 import { Container, TypeProps } from "./styles";
 
 interface Props extends TextInputProps {
     type?: TypeProps;
+    ref?: React.Ref<TextInput> | undefined;
 }
 
-const Input: React.FC<Props> = ({ type = "primary", ...rest }) => {
-    return <Container type={type} {...rest} />;
-};
+const Input: React.FC<Props> = forwardRef(
+    ({ type = "primary", ...rest }, ref) => {
+        return <Container ref={ref} type={type} {...rest} />;
+    }
+);
 
 export default Input;
